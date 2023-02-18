bfbe5528d7cfc5872703fb64f353fdc0edc722f6
Robert Perlinski <rperlinski@gmail.com>
2023-02-18 02:57:58 +0100

    One view file on pug engine

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'REST API' })
 })
+
+app.get('/hello', function(req, res, next) {
+  res.send("Hello World form Czestochowa :D");
+});
+
 app.listen(process.env.PORT || 3000)
